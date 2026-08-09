e8246b952442600ee53bb309073c6559f258f663
AnikhetNair <anikhetnairqppw@gmail.com>
2026-08-09 20:21:25 +0530

    Time: 27 ms (89.10%) | Memory: 12.4 MB (19.64%) - LeetSync

--- 2752-sum-multiples/sum-multiples.py
+++ 2752-sum-multiples/sum-multiples.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def sumOfMultiples(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
         count=0
         for i in range(n+1):
             if i%3==0 or i%5==0 or i%7==0:
