5ff30f8ae465f21ff1a75402d81ba30143be6db3
AnikhetNair <anikhetnairqppw@gmail.com>
2026-08-23 20:44:21 +0530

    Time: 11 ms (86.84%) | Memory: 12.3 MB (51.40%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -4,14 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        count=0
-        while n>1:
-            if n%2==0:
-                count+=n//2
-                n=n//2
-            else:
-                count+=(n-1)//2
-                n=(n//2)+1
-        return count
+        return n-1
 
         
