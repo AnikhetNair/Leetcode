659b81e3b6bb0d463aa00344d1c98b23577e1675
AnikhetNair <anikhetnairqppw@gmail.com>
2026-08-26 17:13:11 +0530

    Time: 95 ms (87.68%) | Memory: 18.9 MB (71.01%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type piles: List[int]
         :rtype: int
         """
-        sm=0
+      
         piles.sort()
-        k=len(piles)//3
-        for i in range(1,k+1):
-            sm+=piles[-2*i]
-        return sm
+        
+        return sum(piles[len(piles) // 3 :: 2])
         
