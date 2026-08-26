86319e0be2cc8b79aa72835128701a0a8c9cc492
AnikhetNair <anikhetnairqppw@gmail.com>
2026-08-26 19:40:07 +0530

    Time: 2 ms (37.78%) | Memory: 12.3 MB (66.67%) - LeetSync

---
+++ 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxContainers(self, n, w, maxWeight):
+        """
+        :type n: int
+        :type w: int
+        :type maxWeight: int
+        :rtype: int
+        """
+        return min(n*n,maxWeight//w)
+        
